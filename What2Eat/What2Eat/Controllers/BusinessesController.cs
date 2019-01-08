952aebe58e7004bc9c5e26a1de5b18e222b1973b
zanderlx <lsaplan@gmail.com>
2019-01-07 20:37:39 -0800

    Integrated front-end interactions

--- What2Eat/What2Eat/Controllers/BusinessesController.cs
+++ What2Eat/What2Eat/Controllers/BusinessesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http;
 using What2Eat.Services;
 
@@ -8,15 +9,23 @@ namespace What2Eat.Controllers
     {
         // Object that contains data pertaining to the Yelp API response
         YelpAPI Api = new YelpAPI();
+        Random rand = new Random();
 
         // GET: api/Businesses
-        public IEnumerable<Business> Get(string latitude, string longitude)
+        public List<string> Get(string latitude, string longitude)
         {
             Api.UserLatitude = latitude;
             Api.UserLongitude = longitude;
             Api.FullApiResponse = Api.ConsumeYelpApi().Result;
             Api.RootObject = Api.GetBusinesses();
-            return Api.RootObject.businesses;
+            IList<Business> businesses = Api.RootObject.businesses;
+            int index = rand.Next(businesses.Count);
+            List<string> details = new List<string>();
+            details.Add(businesses[index].name);
+            details.Add(businesses[index].image_url);
+            details.Add(businesses[index].location.display_address[0] + " " + businesses[index].location.display_address[1]);
+       
+            return details;
         }
 
         // GET: api/Businesses/5
@@ -30,7 +39,6 @@ namespace What2Eat.Controllers
         // PUT: api/Businesses/5
         public void Put(int id, [FromBody]string value)
         {
-
         }
 
         // DELETE: api/Businesses/5
